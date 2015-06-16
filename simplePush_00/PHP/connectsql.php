b429d17355b1e7e665d0b5cdc78b847ae9d12ddc
SammaYang <sbir0106@gmail.com>
2015-06-16 13:25:12 +0800

    連接資料庫改為PDO方式

--- simplePush_00/PHP/connectsql.php
+++ simplePush_00/PHP/connectsql.php
@@ -1,8 +1,20 @@
 <?php
-$link = mysql_connect("192.168.0.12","root","root") or exit (mysql_error());
-mysql_query("set names utf8" );
-if($link)
-{
-	mysql_select_db("userdbs",$link) or exit("...".mysql_error());
-}
+	$db = new PDO("mysql:host=192.168.0.12;dbname=userdbs;port:3306","root","root");
+	$db->exec("set names utf8");
+	if (!$db) {
+		exit("...connect database error ".$db->errorCode().":".$db->errorInfo());
+	} 
+	
+	// test db select
+	/*
+	echo "connect PDO";
+	$result = $db->query("select * from users");
+	while ( $row = $result->fetch() ) {
+		echo "member_No : {$row['member_No']}<br>";
+		echo "member_id : {$row['member_id']}<br>";
+		echo "member_name : {$row['member_name']}<br>";
+		echo "device_token : {$row['device_token']}<br>";
+		echo "<HR>";
+	}
+	*/
 ?>
