003b6fabb3e4ca47b8222c8cb8b5e6cfcf409b24
James <hsiang100@gmail.com>
2015-06-20 18:37:51 +0800

    修改 pushNotification.php(初版)，實作 new_id & msg & device_token 寫入 table `news`。 have_read 狀態由 0 改 1，尚未完成。

--- simplePush_00/PHP/pushNotification.php
+++ simplePush_00/PHP/pushNotification.php
@@ -1,27 +1,22 @@
 <?php
 require_once("connectsql.php");
-require("push_config.php");
-
-// Put your private key's passphrase here:
-// $passphrase = 'twtm2015';
-//$deviceToken = '1ec0f3489a33feaf98d0fc7b75ae8ec7818b55f051c54252b5497ca70920139c';//JamesID
+require_once("push_config.php");
 
 $message = $_POST["msg"];
-$devicetoken = $_POST["device_token"];
 $lists = $_POST["list"];
 
-// echo "$message</br>, $devicetoken</br> $CheckboxGroup1</br>";
+// echo "$message</br>";
 
-// foreach($lists as $value)
+// foreach($lists as $member_id)
 // {
-// 	echo "checked: $value</br>";
+// 	echo "checked: $member_id</br>";
 // }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 $ctx = stream_context_create();
 stream_context_set_option($ctx, 'ssl', 'local_cert', 'apns-dev.pem');
-stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+stream_context_set_option($ctx, 'ssl', 'passphrase', $push_config['development']['push_server']);
 
 // Open a connection to the APNS server
 $fp = stream_socket_client(
@@ -31,26 +26,49 @@ $fp = stream_socket_client(
 if (!$fp) exit("<p>Failed to connect: $err $errstr </p>");
 echo "<p>Connected to APNS</p>";
 
+// 透過 member_id 查詢要推播對象的 device_token, mem_no
 foreach ($lists as $value) {
-	$result = $db->query("select device_token from users where member_id='$value'");
-// 	$sql="select device_token from users where member_id='$value' ";
-// 	$result=mysql_query($sql,$link);
-	while($post=$result->fetch()){
-		if ($post[0]) 
+	$result=$db->query("SELECT device_token,mem_no FROM users WHERE member_id='$value'");
+	while($temp=$result->fetch()){
+		if ($temp[0]) 
 		{
-			$deviceToken = $post[0];
+			$deviceToken = $temp[0];
+			$memNo = $temp[1];
 		}
 	}
-
-	if ($deviceToken) {
-
+	if ($deviceToken && $memNo) {
+		// 組成 news_id (PK)：mem_no (2碼) + 5碼流水號
+		$result = $db->query("SELECT count(news_id) FROM news");
+		$temp = $result->fetch();
+		if (!$temp){
+			$newsId = $memNo."00000";	
+		}else {
+			$newsIdCount = $temp[0];
+		}
+		$newsIdCount++;
+		$newsId = $memNo.sprintf("%05s",$newsIdCount);
+		
+// 		echo  "newsIdNumber:$newsIdNumber<br>";
+// 		echo  "newsId:$newsId<br>";
+		
+		// 寫入資料庫 table `news` news_id & device_token & msg
+		$result = $db->prepare("INSERT INTO news (news_id, msg, device_token) VALUES (?, ?, ?)");
+		$result->execute(array((string)$newsId, $message, $deviceToken));
+// 		$stmt->bindParam(1, $newsId);
+// 		$stmt->bindParam(2, $message);
+// 		$stmt->bindParam(3, $deviceToken);
+		
 		// Create the payload body
+		$result = $db->prepare("SELECT COUNT(news_id) FROM news WHERE have_read = 0 AND news_id LIKE '$memNo%'");
+		$result->execute();
+		$temp = $result->fetch();
+		$badge = $temp[0];
+
 		$body['aps'] = array(
 				'alert' => $message,
 				'sound' => 'default',
-				'badge' => 2
+				'badge' => (int)$badge
 			);
-
 		// Encode the payload as JSON
 		$payload = json_encode($body);
 
@@ -72,8 +90,6 @@ foreach ($lists as $value) {
 			echo "Message successfully delivered.</br>";
 	}
 }
-
-
 // Close the connection to the server
 fclose($fp);
 
