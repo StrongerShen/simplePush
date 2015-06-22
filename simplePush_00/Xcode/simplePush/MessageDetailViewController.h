7f1e587876239f8c56cd9c054aaa87fa321aed3a
GeorgeLuo <georgeluo@tomingroup.com>
2015-06-23 01:32:48 +0800

    新增完整訊息內容頁面

---
+++ simplePush_00/Xcode/simplePush/MessageDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessageDetailViewController.h
+//  simplePush
+//
+//  Created by 羅祐昌 on 2015/6/23.
+//  Copyright (c) 2015年 Samma.Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageDetailViewController : UIViewController
+
+@end
