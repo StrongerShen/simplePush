3cc6bf1c5b4e8650319ec79e497909b49aac2f15
GeorgeLuo <cookiebox33@gmail.com>
2015-06-25 15:41:22 +0800

    Initial Commit

---
+++ simplePush_00/Xcode/simplePush/simplePush/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  simplePush
+//
+//  Created by GeorgeLuo on 2015/6/25.
+//  Copyright (c) 2015年 TOMIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
