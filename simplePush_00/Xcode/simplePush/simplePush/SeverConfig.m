de6fe44d16202b75e270511c43a09c60d591a0e5
GeorgeLuo <cookiebox33@gmail.com>
2015-06-25 18:42:21 +0800

    [Big Change] v0.0.1 可執行擋

---
+++ simplePush_00/Xcode/simplePush/simplePush/SeverConfig.m
@@ -0,0 +1,13 @@
+//
+//  SeverConfig.m
+//  simplePush
+//
+//  Created by GeorgeLuo on 2015/6/25.
+//  Copyright (c) 2015年 TOMIN. All rights reserved.
+//
+
+#import "SeverConfig.h"
+
+@implementation SeverConfig
+
+@end
