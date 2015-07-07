34385b913dd5b2919e318076260daa63af0d6a65
TOMIN.PatrickChan <patrickchan@tomingroup.com>
2015-07-07 17:51:43 +0800

    新增登入頁面

--- Android/simplePush/app/src/main/java/com/tomin/patrickchan/simplepush/MainActivity.java
+++ Android/simplePush/app/src/main/java/com/tomin/patrickchan/simplepush/MainActivity.java
@@ -1,41 +1,117 @@
 package com.tomin.patrickchan.simplepush;
 
-import android.support.v7.app.ActionBarActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.preference.PreferenceManager;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String TAG = "MainActivity";
+
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+    private ProgressBar mRegistrationProgressBar;
+    private EditText mEdtUserName, mEdtDeviceName;
+    private Button mBtnSubmit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mRegistrationProgressBar = (ProgressBar) findViewById(R.id.registrationProgressBar);
+        mEdtUserName = (EditText)findViewById(R.id.edtUserName);
+        mEdtDeviceName = (EditText)findViewById(R.id.edtDeviceName);
+        mBtnSubmit = (Button)findViewById(R.id.btnSubmit);
 
+        mBtnSubmit.setOnClickListener(btnSubmitOnClick);
 
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                mRegistrationProgressBar.setVisibility(ProgressBar.GONE);
+                SharedPreferences sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(context);
+
+                boolean sentToken = sharedPreferences
+                        .getBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false);
+                if (sentToken) {
+                    mBtnSubmit.setVisibility(View.VISIBLE);
+                } else {
+                    Toast.makeText(MainActivity.this, getString(R.string.token_error_message), Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+
+        if (checkPlayServices()) {
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
+        }
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        super.onPause();
+    }
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private View.OnClickListener btnSubmitOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String strUserName = mEdtUserName.getText().toString();
+            String strDeviceName = mEdtDeviceName.getText().toString();
+
+            if(strUserName.equals("") || strDeviceName.equals("")){
+                Toast.makeText(MainActivity.this,"請勿留白",Toast.LENGTH_LONG).show();
+            }else {
+                //Toast.makeText(MainActivity.this,sharedPreferences.getString("DEVICE_TOKEN",""),Toast.LENGTH_LONG).show();
+            }
         }
-
-        return super.onOptionsItemSelected(item);
-    }
+    };
 }
---
+++ Android/simplePush/app/src/main/java/com/tomin/patrickchan/simplepush/QuickstartPreferences.java
@@ -0,0 +1,9 @@
+package com.tomin.patrickchan.simplepush;
+
+/**
+ * Created by patrickchan on 15/7/7.
+ */
+public class QuickstartPreferences {
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+}
