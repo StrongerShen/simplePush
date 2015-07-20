cb71db7ec253946a817da895e7efe0b1b3e68896
TOMIN.PatrickChan <patrickchan@tomingroup.com>
2015-07-20 18:03:25 +0800

    [Android] - 1. 介面樣式套用 		  2. 新聞內容頁面 toolbar 加上BackButton

--- Android/simplePush/app/src/main/java/com/tomin/patrickchan/simplepush/VNewsDetailActivity.java
+++ Android/simplePush/app/src/main/java/com/tomin/patrickchan/simplepush/VNewsDetailActivity.java
@@ -5,7 +5,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
@@ -19,41 +25,55 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
  * Created by patrickchan on 15/7/14.
  */
-public class VNewsDetailActivity extends Activity {
+public class VNewsDetailActivity extends AppCompatActivity {
 
     private static final String TAG = "VNewsDetailActivity";
 
-    private TextView mTxtNewsId, mTxtNewsContent, mTxtNewsTitle, mTxtSendTime;
+    private String strNewsId,strNewsTitle,strSendtime;
+
+    private SimpleAdapter newsArrayAdapter;
+    private ArrayList<HashMap<String, String>> news;
+    private ListView newsDetailView;
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.news_detail_toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               onBackPressed();
+            }
+        });
+
         findView();
         showNewsContent();
     }
 
     private void findView(){
-        mTxtNewsId = (TextView)findViewById(R.id.txtNewsId);
-        mTxtNewsTitle = (TextView)findViewById(R.id.txtNewsTitle);
-        mTxtSendTime = (TextView)findViewById(R.id.txtSendTime);
-        mTxtNewsContent = (TextView)findViewById(R.id.txtNewsContent);
+
     }
 
     private void showNewsContent(){
         Bundle bundle = VNewsDetailActivity.this.getIntent().getExtras();
-        String strNewsId = bundle.getString("newsId");
-        String strNewsTitle = bundle.getString("newsTitle");
-        String strSendtime = bundle.getString("sendTime");
-        mTxtNewsId.setText(strNewsId);
-        mTxtNewsTitle.setText(strNewsTitle);
-        mTxtSendTime.setText(strSendtime);
+        strNewsId = bundle.getString("newsId");
+        strNewsTitle = bundle.getString("newsTitle");
+        strSendtime = bundle.getString("sendTime");
+
+        news =new ArrayList<HashMap<String,String>>();
 
         Log.d(TAG,strNewsId+strNewsTitle+strSendtime);
 
@@ -72,7 +92,24 @@ public class VNewsDetailActivity extends Activity {
 
                     String strAppServerRegister = jsAppServerResponse.getString("fullMsg");
 
-                    mTxtNewsContent.setText(strAppServerRegister);
+                    HashMap<String, String> item = new HashMap<String, String>();
+
+                    item.put("newsTitle",strNewsTitle);
+                    item.put("sendTime",strSendtime);
+                    item.put("fullMsg", strAppServerRegister);
+
+                    news.add(item);
+                    newsDetailView = (ListView)findViewById(R.id.newsDetailView);
+                    newsArrayAdapter = new SimpleAdapter(
+                            getApplicationContext(),
+                            news,R.layout.news_detail_item,
+                            new String[]{"newsTitle","sendTime","fullMsg"},
+                            new int[]{
+                                    R.id.txtNewsDetailItem1,
+                                    R.id.txtNewsDetailItem2,
+                                    R.id.txtNewsDetailItem3,});
+
+                    newsDetailView.setAdapter(newsArrayAdapter);
 
 
                 }catch (JSONException e){
@@ -92,7 +129,7 @@ public class VNewsDetailActivity extends Activity {
             protected HashMap<String, String> getParams()
                     throws AuthFailureError {
                 HashMap<String, String> hashMap = new HashMap<String, String>();
-                hashMap.put("news_id", mTxtNewsId.getText().toString());
+                hashMap.put("news_id", strNewsId);
 
 
                 return hashMap;
---
+++ Android/simplePush/app/src/main/java/com/tomin/patrickchan/simplepush/VolleyNetWorking.java
@@ -0,0 +1,8 @@
+package com.tomin.patrickchan.simplepush;
+
+/**
+ * Created by patrickchan on 15/7/20.
+ */
+public class VolleyNetWorking {
+
+}
